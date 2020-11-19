e97402b921ca04d8ba1b3cdd94ff722e1e45ffc9
KOMURA Takaaki <komura.takaaki.3v@kyoto-u.ac.jp>
2020-11-19 09:37:53 +0000

    add sub-commands, logging

--- script/ufw-update.sh
+++ script/ufw-update.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
 MAGIC_WORD='from https://api.fastly.com/public-ip-list'
+DEBUG=${DEGUG:-FALSE}
+CMD=$1
+
+log(){
+    logger -p local0.info --id=$$ "$*"
+    decho "log:" "$*"
+}
+
+decho(){
+    if [ "$DEBUG" != "FALSE" ]; then
+	echo "$*" > /dev/stderr
+    fi
+}
+
 
 ufw_status(){
     sudo ufw status numbered | grep "${MAGIC_WORD}"
@@ -14,28 +28,53 @@ fastly_address(){
     curl -s -o - https://api.fastly.com/public-ip-list | ./jq.py
 }
 
-fastly_ips_old=$(tempfile) || exit
-fastly_ips_new=$(tempfile) || exit
-trap "rm -f -- '$fastly_ips_old' '$fastly_ips_new'" EXIT
 
-ufw_status | get_address | sort > ${fastly_ips_old}
-fastly_address | sort > ${fastly_ips_new}
+ufw_diff(){
+    fastly_ips_old=$(tempfile) || exit
+    fastly_ips_new=$(tempfile) || exit
+    trap "rm -f -- '$fastly_ips_old' '$fastly_ips_new'" EXIT
 
-DIFF=`diff -u0 ${fastly_ips_old} ${fastly_ips_new} | tail -n +3`
-rm -f -- "$fastly_ips_old" "$fastly_ips_new"
-trap - EXIT
+    ufw_status | get_address | sort > ${fastly_ips_old}
+    fastly_address | sort > ${fastly_ips_new}
 
-echo "${DIFF}" | tee /dev/stderr | while read line; do
-    type=`echo $line | cut -b 1`
-    ip=`echo $line | cut -b 2-`
-    case ${type} in
-	"+" )
-	    echo sudo ufw allow in from ${ip} to any port ssh comment "${MAGIC_WORD}"
-	         sudo ufw allow in from ${ip} to any port ssh comment "${MAGIC_WORD}"
-	    ;;
-	"-" )
-	    echo sudo ufw delete allow in from ${ip} to any port ssh
-	    yes| sudo ufw delete allow in from ${ip} to any port ssh
-	    ;;
-    esac
-done
+    diff -u0 ${fastly_ips_old} ${fastly_ips_new} | tail -n +3
+    rm -f -- "$fastly_ips_old" "$fastly_ips_new" > /dev/null
+    trap - EXIT
+}
+
+ufw_apply(){
+    while read line; do
+	type=`echo $line | cut -b 1`
+	ip=`echo $line | cut -b 2-`
+	case ${type} in
+	    "+" )
+		log sudo ufw allow in from ${ip} to any port ssh comment "${MAGIC_WORD}"
+	        sudo ufw allow in from ${ip} to any port ssh comment "${MAGIC_WORD}"
+		;;
+	    "-" )
+		log sudo ufw delete allow in from ${ip} to any port ssh
+		yes| sudo ufw delete allow in from ${ip} to any port ssh
+		;;
+	esac
+    done
+}
+
+log $0 ${CMD}
+case ${CMD} in
+    local)
+	ufw_status
+	;;
+    fetch)
+	fastly_address
+	;;
+    diff)
+	ufw_diff
+	;;
+    apply)
+	ufw_diff | ufw_apply
+	;;
+    *)
+	ufw_status
+	;;
+esac
+log finished
